9aea5f8fc1bf5aa3c97d9ae25ea59be869363d57
dv-sharma <divyam.sharma3@gmail.com>
2022-05-30 14:28:17 +0530

    adding function scripts

---
+++ functions/Global_localvar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+functionfirst(){
+    variable_first=5
+}
+
+
+echo $variable_first
+
+functionfirst
+
+echo $variable_first
+
+## scope of global variable is outside the brackets.
---
+++ functions/basicfunction.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+helloWorld(){
+    echo "Hello, World!"
+}
+
+
+## either use () or use function word in syntax or use bash script.sh
+
+# calling the function
+
+helloWorld
+
+function helloword
+{
+    echo " Hello "$1" "
+}
+
+helloword Divyam
